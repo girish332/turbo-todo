7c96e482469f29d9dc29d5ac970fd3e46185d8ca
Girish Kulkarni <girish332@pop-os.localdomain>
2020-12-04 18:28:31 +0530

    Changed error util

--- utils/err.go
+++ utils/err.go
@@ -5,12 +5,22 @@ import (
 	"net/http"
 )
 
+type APIError struct {
+	Message    string `json:"err_message"`
+	StatusCode int    `json:"status_code"`
+}
+
 // JSONError function to return error
-func JSONError(w http.ResponseWriter, err interface{}, code int) {
+func JSONError(w http.ResponseWriter, err error, code int, message string) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(err)
+	response := map[string]string{"error": message}
+	res, _ := json.Marshal(response)
+	w.Write(res)
+	// json.NewEncoder(w).Encode(err.Error())
+	// er, _ := json.Marshal(err)
+	// io.WriteString(w, `{"error": "Wrong request"}`)
+
 }
 
 // JSONOk func for status 200 requeests
